67eca371d408318951b11b5d6124ed8da436356f
Aditya <adityapratap@poshmark.com>
2022-03-03 13:19:40 +0530

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Aditya Pratap Singh Rajput on 03/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
